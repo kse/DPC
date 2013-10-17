6833a21d350ae4e1f314f4138fa24786bdcbbaa0
Kasper Eenberg <kse@mahavira.dk>
2013-10-17 13:04:47 +0200

    Added some describing comments

--- src/kmeans_cpu_impl.h
+++ src/kmeans_cpu_impl.h
@@ -1,4 +1,18 @@
+/*
+ * Calculates the total euclidian distance from all points x ∈ X,
+ * to the point c ∈ C closest to x 
+ */
 float cost(dps_t *X, datapoint_array_t *C);
+
+/*
+ * Same as above, but taking different parameters.
+ * Should be reworked if i have the time left over.
+ */
+float costpp(datapoint_array_t *X, datapoint_array_t *C);
+
+/*
+ * Distance from x to the closest element c ∈ C
+ */
 float dist(dp_t *x, datapoint_array_t *C);
 
 /*
@@ -12,5 +26,3 @@ datapoint_array_t *kmeans_parallel_init(dps_t *X, int k);
  */
 void kmeanspp_init(datapoint_array_t *X, datapoint_array_t *C, int k);
 void kmeanspp_impl(dps_t *X, datapoint_array_t *C);
-
-float costpp(datapoint_array_t *X, datapoint_array_t *C);
