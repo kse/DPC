e573c1108b67a40f1f091dc40a12661a7afe2f42
Kasper Eenberg <kse@mahavira.dk>
2013-10-27 19:52:57 +0100

    Update benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -2,18 +2,18 @@
 
 OUT=bm.out
 
-CONFIG=("--cpu" "--blocksize 256")
+CONFIG=("--cpu" "--blocksize=256" "--blocksize=512" "--blocksize=1024")
 
 :>$OUT
 
-for i in 10 25 50 100 200 500 1000 1500; do 
-	for cfg in $CONFIG; do
+for i in 10 25 50 100 200 500 1000 1400; do 
+	for cfg in ${CONFIG[*]}; do
 		echo "k = $i, $cfg" >> $OUT;
+
 		for j in `seq 1 10`; do
-			#echo "k = $i, run $j:" >> $OUT;
 			{ ./kmeans -k $i $cfg; } >> $OUT 2>&1;
-				#echo -e "\n\n" >> $OUT; 
-			done
-			echo -e "\n" >> $OUT; 
 		done
+
+		echo -e "\n" >> $OUT; 
 	done
+done
